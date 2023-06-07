bbf70cc2c1f6c9c2c0b48963fccee73e73fb6915
Manouchehr <manouchehr.sgh@gmail.com>
2023-06-07 18:05:01 +0330

    change terraform to manage ip addresses

--- provision/proxmox/terraform/main.tf
+++ provision/proxmox/terraform/main.tf
@@ -1,36 +1,25 @@
-# Define the Proxmox virtual machine resources
 resource "proxmox_vm_qemu" "my_vm" {
   count       = var.vm_count
   name        = "vm${count.index+1}"
-  target_node = "pve" 
-  os_type      = "l26"
-  memory      = 8192
-  cores       = 2
-  # description = "My VM ${count.index+1}"
+  desc        = "focal server description"
+  target_node = "pve"
+  scsihw      = "virtio-scsi-single"
+  clone       = "template"
 
-  storage     = "hdd"  # Replace with your storage ID
-  clone       = "template"   # Replace with your template name
+  disk {
+    size    = "15G"
+    type    = "scsi"
+    storage = "local-lvm"
+  }
+  sockets = 4
+  cores   = 2
+  memory  = 8192
 
   network {
-    model = "virtio"
-    bridge    = "vmbr0"
+    model  = "virtio"
+    bridge = "vmbr0"
   }
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "ip addr | grep 'inet' | awk '{print $2}' >> b.txt"
-  #   ]
-  # }
-  # connection {
-  #     type        = "ssh"
-  #     user        = var.vm_ssh_user
-  #     private_key = file(var.vm_ssh_private_key)
-  #     host        = self.ipv4_address
-  #   }
+  
+  ipconfig0 = "ip=192.168.1.23${count.index+1}/24,gw=192.168.1.1"
+
 }
-# data "external" "my_vm" {
-#   count = var.vm_count
-#   name  = "vm1"#external.my_vm[count.index].name
-# }
-# output "vm_ip_addresses" {
-#   value = [for vm in data.external.my_vm : vm.ip_address]
-# }
--- provision/proxmox/terraform/terraform.tfvars
+++ provision/proxmox/terraform/terraform.tfvars
@@ -1,6 +1,4 @@
 # terraform.tfvars
 
-pm_user     = "root@pam"
-pm_password = "mohamadi@123"
-#PM_API_TOKEN_ID="APItoken"
-# PM_API_TOKEN_SECRET="afcd8f45-acc1-4d0f-bb12-a70b0777ec11"
+pm_user     = "<username>"
+pm_password = "<password>"
